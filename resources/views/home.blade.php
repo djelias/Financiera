29a9512faf1cab7b597d3a35154f605fcec2c0cf
unknown <migueluma11@hotmail.com>
2021-05-13 09:09:05 -0600

    Nueva pagina de inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+ @extends('layout')
+ @section('container')
+ <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="post-preview">
+                        <a href="post.html">
+                            <h2 class="post-title">Desarrollado para la gestión de las especies naturales de la biodiversidad salvadoreña</h2>
+                            <h3 class="post-subtitle">Más de dos mil especies registradas. </h3>
+                        </a>
+                        <p class="post-meta">
+                            Sitio creado para la
+                            <a href="#!">Escuela de Biologia</a>
+                            de la Universidad de El Salvador
+                        </p>
+                    </div>
+                    <hr />
+                   
+                    <hr />
+                    <!-- Pager-->
+                    <div class="clearfix"><a class="btn btn-primary float-right" href="#!">Noticias →</a></div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    @endsection
